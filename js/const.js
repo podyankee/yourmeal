fc2b72549d74d760c014d165f1fc79d310431a70
podyankee <podyankee@gmail.com>
2022-12-16 04:15:25 +0000

    api url changed

--- js/const.js
+++ js/const.js
@@ -1,2 +1,3 @@
-export const API_URL = 'http://localhost:3024';
+export const API_URL = 'https://foremost-climbing-beach.glitch.me';
+// export const API_URL = 'http://localhost:3024';
 export const PREFIX_PRODUCT = '/api/product';
