d66ccdd69adfe3fa610a0f1679960c6ae9840ebe
podyankee <podyankee@gmail.com>
2022-12-18 08:03:49 +0000

    project finished

--- js/elements.js
+++ js/elements.js
@@ -19,3 +19,4 @@ export const orderTotalAmount = document.querySelector('.order__total-amount');
 export const order = document.querySelector('.order');
 export const orderWrapTitle = document.querySelector('.order__wrap-title');
 export const orderSubmit = document.querySelector('.order__submit');
+export const modalDeliveryContainer = document.querySelector('.modal-delivery__container');
--- js/orderController.js
+++ js/orderController.js
@@ -1,9 +1,8 @@
-import { modalDeliveryForm } from './elements.js';
+import { modalDeliveryForm, modalDeliveryContainer } from './elements.js';
 import { clearCart } from './cart.js';
-console.log(modalDeliveryForm);
 
 export const orderController = getCart => {
-	modalDeliveryForm.addEventListener('change', () => {
+	const checkDelivery = () => {
 		if (modalDeliveryForm.format.value === 'pickup') {
 			modalDeliveryForm['address-info'].classList.add('modal-delivery__fieldset-input_hide');
 		}
@@ -11,7 +10,9 @@ export const orderController = getCart => {
 		if (modalDeliveryForm.format.value === 'delivery') {
 			modalDeliveryForm['address-info'].classList.remove('modal-delivery__fieldset-input_hide');
 		}
-	});
+	};
+
+	modalDeliveryForm.addEventListener('change', checkDelivery);
 
 	modalDeliveryForm.addEventListener('submit', e => {
 		e.preventDefault();
@@ -23,8 +24,30 @@ export const orderController = getCart => {
 			body: JSON.stringify(data),
 		})
 			.then(response => response.json())
-			.then(data => {
+			.then(response => {
 				clearCart();
+
+				modalDeliveryContainer.innerHTML = `
+					<h2>Спасибо за заказ!</h2>
+					<h3>Номер вашего заказа ${response.id}</h3>
+					<p>С вами в ближайшее время свяжется наш менеджер ${response.manager}</p>
+					<p>Ваш заказ:</p>
+				`;
+
+				const ul = document.createElement('ul');
+				data.order.forEach(item => {
+					ul.insertAdjacentHTML(
+						'beforeend',
+						`
+						<li>${item.id}</li>
+					`,
+					);
+				});
+
+				modalDeliveryContainer.insertAdjacentElement('beforeend', ul);
+
+				// modalDeliveryForm.reset();
+				// checkDelivery();
 			});
 	});
 };
